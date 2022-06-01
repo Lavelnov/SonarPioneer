fc869c7ded3bc1852305e6d22cbd87115965563b
Lavelnov <104739162+Lavelnov@users.noreply.github.com>
2022-06-01 10:53:45 -0500

    Sonar 2.0
    The sonar with library and sonar selector
    Use the constructor 
     SonarPioneer Sonar(S0,S1,S2,trigger,Inhibits,Echo);
    Function:
    Sonar.Read(Number of sonar);
    
    S0,S1,S2 are the pins for selecting the phisical sonar.
    Trigger sends the sonar signal out.
    Ihnibits Changes the sonar state from sending to reading
    Echo is attached to an analog pin and is the response of the signal, it reaches only 3.7-4.2 v
    connection Diagram: 
                       ________
    GND          |9       10|  Echo
     5v           | |7         8|   GND
     Trigger    | |5         6|  5v
    S2            | | 3       4 |     Ihnibits
    S0              | 1 ____2 |     S1

--- sonar.ino
+++ sonar.ino
@@ -1,76 +1,24 @@
-
-unsigned long duration;
-#define tri 8
-#define echo 18
-#define inh 10
+#include "SonarPioneer.h"
+  SonarPioneer Sonar(3,4,5,8,9,A0);
 void setup() {
-  //selector
-  pinMode(3,OUTPUT);
-  pinMode(4,OUTPUT);
-  pinMode(5,OUTPUT);
-//trigger
-  pinMode(8,OUTPUT);
-//echo
-  pinMode(A0,INPUT);
-  //inhibits
-  pinMode(10,OUTPUT);
-
-  //Selector de sensor
-  digitalWrite(3,LOW);
-  digitalWrite(4,LOW);
-  digitalWrite(5,LOW);
-  
-  digitalWrite(8,LOW);
-  //digitalWrite(9,LOW);
-  digitalWrite(10,LOW); 
-
-  long duration=0;
   delay(3000);
   Serial.begin(9600);
-
 }
 
-  int flag = 0;
-  int ecpin=0;
 void loop() {
+  int aux=0;
+  int i=0;
+  for(i=0; i<=7;i++){
+  aux=Sonar.Read(i);
+  Serial.print("S");
+  Serial.print(i);
+  Serial.print("   ");
+  Serial.print(aux);
+  Serial.print("   |   ");
+  if(i==7){
+    Serial.println(" ");
+  }
 
-  //trigger off
-  digitalWrite(tri,LOW);
-  
-  digitalWrite(inh,LOW);
-  delayMicroseconds(1200);
-  
-  digitalWrite(tri,HIGH);
-  delayMicroseconds(800);
-  digitalWrite(inh,HIGH);
-  
- // delayMicroseconds(38000);
-
-  //digitalWrite(tri,LOW);
-  //digitalWrite(inh,LOW);
-
-  flag=1;
-  duration=8;
-  do{
-    delayMicroseconds(1);
-    duration++;
-    ecpin=analogRead(A0);
-    if(ecpin>600){
-      break;
-      flag=0;
-    }
-    if(duration>2000){
-      break;
-      flag=0;
-    }
-  }while(flag=1);
-
- // duration = pulseIn(18, HIGH);
-  Serial.println(duration* 3.4 / 2,"    ");
-   // delay(5);
-  //Serial.println(ecpin);
- // delay(38);
-  delay(500);
-
- // delay(2000);
+  }
+    //delay(100);
 }
